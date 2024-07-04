b2c5d4208ec87e22098708226b3edeff937bca2d
Samuel Wambeo <samuelwambeo9@gmail.com>
2024-07-04 22:34:32 +0300

    complete

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+function App() {
+ useEffect(()=>{
+  fetch('/movies')
+   .then(response=>response.json())
+   .then((movies)=>console.log(movies))
+ },[])
+  return <h1>Check the console for a list of movies!</h1>
+}
+
+export default App;
